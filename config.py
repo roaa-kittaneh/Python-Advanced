af7d275f91bb0297cacfe2d2dc8118cf7d21718c
roaa-kittaneh <roaakittaneh2004@gmail.com>
2026-03-07 03:39:27 +0300

    add .env

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'default-secret-key'
+
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'students.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
